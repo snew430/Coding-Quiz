aca9c9dfc994a38420ef8957071312fb9ef54fda
snew430 <seanpnew@hotmail.com>
2021-12-31 06:51:03 -0500

    changed function properties

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,14 +13,12 @@ var countdown = function (duration, display) {
   setInterval(function () {
     minutes = parseInt(time / 60, 10);
     seconds = parseInt(time % 60, 10);
-    // milliseconds = parseInt(time, 10);
 
     minutes = minutes < 10 ? "0" + minutes : minutes;
     seconds = seconds < 10 ? "0" + seconds : seconds;
 
     display.innerText = minutes + ":" + seconds;
-    // console.log(display.innerText);
-    // console.log(time);
+
 
     if (--time < 0) {
       time = duration;
@@ -35,63 +33,96 @@ var countdown = function (duration, display) {
 
 //QUIZ FUNCTIONS BELOW
 
-var quizBox = document.querySelector(".quiz-box");
-
 var questions = [
   {
     Question: "Question 1",
-    Option1: ["Answer1", "wrong"],
-    Option2: ["Answer2", "wrong"],
-    Option3: ["This is the correct answer(3)", "correct"],
-    Option4: ["Answer4", "wrong"],
+    Options: ["Answer1", "Answer2", "This is the correct answer(3)", "Answer4"],
+    Correct: "This is the correct answer(3)",
   },
   {
     Question: "Question 2",
-    Option1: ["Answer1", "wrong"],
-    Option2: ["This is the correct answer(2)", "correct"],
-    Option3: ["Answer3", "wrong"],
-    Option4: ["Answer4", "wrong"],
+    Options: ["Answer1", "This is the correct answer(2)", "Answer3", "Answer4"],
+    Correct: "This is the correct answer(2)",
   },
   {
     Question: "Question 3",
-    Option1: ["This is the correct answer(1)", "correct"],
-    Option2: ["Answer2", "wrong"],
-    Option3: ["Answer3", "wrong"],
-    Option4: ["Answer4", "wrong"],
+    Options: ["This is the correct answer(1)", "Answer2", "Answer3", "Answer4"],
+    Correct: "This is the correct answer(1)",
   },
   {
     Question: "Question 4",
-    Option1: ["Answer1", "wrong"],
-    Option2: ["This is the correct answer(2)", "correct"],
-    Option3: ["Answer3", "wrong"],
-    Option4: ["Answer4", "wrong"],
+    Options: ["Answer1", "This is the correct answer(2)", "Answer3", "Answer4"],
+    Correct: "This is the correct answer(2)",
   },
   {
     Question: "Question 5",
-    Option1: ["Answer1", "wrong"],
-    Option2: ["Answer2", "wrong"],
-    Option3: ["Answer3", "wrong"],
-    Option4: ["This is the correct answer(4)", "correct"],
+    Options: ["Answer1", "Answer2", "Answer3", "This is the correct answer(4)"],
+    Correct: "This is the correct answer(4)",
   },
 ];
 
+var thisQuestionIndex = 0;
+
+var behindQuizBox = document.querySelector(".main-body")
+var quizBox = document.querySelector(".quiz-box");
+
+var displayQuestion = document.createElement("div");
+displayQuestion.className = "question-container";
+
+var displayAnswers = document.createElement("div");
+displayAnswers.className = "answer-container";
+
+var userPoints = 0;
+
 // ========FUNCTION THAT PROVIDES THE NEXT QUESTION==========
-var nextQuestion = function (questionAndAnswers) {
-  var currentQuestion = questionAndAnswers.Question;
-  var theAnswers = [
-    questionAndAnswers.Option1,
-    questionAndAnswers.Option2,
-    questionAndAnswers.Option3,
-    questionAndAnswers.Option4,
-  ];
-  var newAnswerOrder = shuffle(theAnswers);
+var nextQuestion = () => {
 
-  console.log(currentQuestion, newAnswerOrder);
+  // Deletes everything inside the quizbox
+  displayQuestion.textContent = "";
+  while(displayAnswers.firstChild) {
+    displayAnswers.removeChild(displayAnswers.firstChild);
+  }
 
-  var userSelection = display(currentQuestion, newAnswerOrder);
+  // Get the question
+  var currentQuestion = questions[thisQuestionIndex].Question;
 
-  return userSelection;
+  // Display the question
+  displayQuestion.textContent = currentQuestion;
+  // Add the question as a child to the quizbox
+  quizBox.appendChild(displayQuestion);
+
+  var newAnswerOrder = shuffle(questions[thisQuestionIndex].Options)
+  console.log(newAnswerOrder)
+
+  // Get and display the options
+  newAnswerOrder.forEach((option, i) => {
+    var answers = document.createElement("div");
+    answers.className = "answer-box";
+    answers.setAttribute("data-id", i);
+    answers.textContent = option;
+    answers.onclick = answerClick;
+    displayAnswers.append(answers);
+    quizBox.appendChild(displayAnswers);
+  });
 };
+
+function answerClick() {
+  if (this.textContent == questions[thisQuestionIndex].Correct) {
+    userPoints += 10;
+    console.log(userPoints, timer);
+    // behindQuizBox.animate()
+  } else {
+    timer -= 20;
+    console.log(userPoints, timer);
+  }
+  thisQuestionIndex++;
+  if (thisQuestionIndex === questions.length) {
+    endGame(userPoints);
+  } else {
+    nextQuestion();
+  }
+}
+
 
 // SHUFFLE AN ARRAY FUNCTION
 var shuffle = function (arr) {
@@ -100,82 +131,30 @@ var shuffle = function (arr) {
     var temp = arr[i];
     arr[i] = arr[newOrder];
     arr[newOrder] = temp;
-    // console.log(arr);
   }
   return arr;
 };
 
-var answerSelection = function (x) {
-  if (x[1] === "correct") {
-    return true;
-  } else {
-    return false;
+// END GAME FUNCTION
+var endGame = function (points) {
+
+  displayQuestion.textContent = "";
+  while(displayAnswers.firstChild) {
+    displayAnswers.removeChild(displayAnswers.firstChild);
   }
+
+  displayQuestion.textContent = "You have finished the game!  You finished with " + points + " points!"
 };
 
-// =========FUNCTION THAT DISPLAYS THE QUESTION ANS ANSWERS PASSED THROUGH IT
-var display = function (question, answers) {
-  var displayQuestion = document.createElement("div");
-  displayQuestion.className = "question-container";
-  displayQuestion.textContent = question;
-
-  var displayAnswers = document.createElement("div");
-  displayAnswers.className = "answer-container";
-
-  var answerOne = document.createElement("div");
-  answerOne.className = "answer-box";
-  answerOne.textContent = answers[0];
-  answerOne.setAttribute("data-id", "answer1");
-  displayAnswers.appendChild(answerOne);
-
-  var answerTwo = document.createElement("div");
-  answerTwo.className = "answer-box";
-  answerTwo.textContent = answers[1];
-  answerTwo.setAttribute("data-id", "answer2");
-  displayAnswers.appendChild(answerTwo);
-
-  var answerThree = document.createElement("div");
-  answerThree.className = "answer-box";
-  answerThree.textContent = answers[2];
-  answerThree.setAttribute("data-id", "answer3");
-  displayAnswers.appendChild(answerThree);
-
-  var answerFour = document.createElement("div");
-  answerFour.className = "answer-box";
-  answerFour.textContent = answers[3];
-  answerFour.setAttribute("data-id", "answer4");
-  displayAnswers.appendChild(answerFour);
-
-  var answer1Button = document.querySelector("#answer1");
-  var answer2Button = document.querySelector("#answer2");
-  var answer3Button = document.querySelector("#answer3");
-  var answer4Button = document.querySelector("#answer4");
-
-  quizBox.appendChild(displayQuestion);
-  quizBox.appendChild(displayAnswers);
-
-  // answer1Button.addEventListener("click", answerSelection())
-};
-
-var endGame = function (points) {};
-
-var startQuiz = function () {
-  var userPoints = 0;
-  for (i = 0; i < questions.length; i++) {
-    var rightOrWrong = nextQuestion(questions[i]);
-    if (rightOrWrong) {
-      userPoints = userPoints + 10;
-    } else {
-      timer = timer - 20;
-    }
-    if (i === questions.length) {
-      alert("THE QUESTIONS ARE OVER");
-      endGame(userPoints);
-    }
-  }
-};
+// START QUIZ FUNCTION
+function startQuiz() {
+  countdown(timer, quizTimer);
+  userPoints = 0;
+  nextQuestion();
+}
 
 var quizButton = document.querySelector("#start-button");
 
-quizButton.addEventListener("click", countdown(timer, quizTimer));
-quizButton.addEventListener("click", startQuiz());
+quizButton.addEventListener("click", () => {
+  startQuiz();
+});
